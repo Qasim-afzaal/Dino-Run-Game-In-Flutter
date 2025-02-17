7ac1b4ad8244f52de3e0818c50f5d441dc4d6d97
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-02-18 02:57:26 +0500

    add dino run

--- lib/game/dino_run.dart
+++ lib/game/dino_run.dart
@@ -16,11 +16,9 @@ import '/models/player_data.dart';
 import '/widgets/pause_menu.dart';
 import '/widgets/game_over_menu.dart';
 
-// This is the main flame game class.
 class DinoRun extends FlameGame with TapDetector, HasCollisionDetection {
   DinoRun({super.camera});
 
-  // List of all the image assets.
   static const _imageAssets = [
     'DinoSprites - tard.png',
     'AngryPig/Walk (36x30).png',
@@ -34,7 +32,6 @@ class DinoRun extends FlameGame with TapDetector, HasCollisionDetection {
     'parallax/plx-6.png',
   ];
 
-  // List of all the audio assets.
   static const _audioAssets = [
     '8BitPlatformerLoop.wav',
     'hurt7.wav',
@@ -48,79 +45,54 @@ class DinoRun extends FlameGame with TapDetector, HasCollisionDetection {
 
   Vector2 get virtualSize => camera.viewport.virtualSize;
 
-  // This method get called while flame is preparing this game.
   @override
   Future<void> onLoad() async {
-    // Makes the game full screen and landscape only.
+    await _initializeGame();
+  }
+
+  Future<void> _initializeGame() async {
     await Flame.device.fullScreen();
     await Flame.device.setLandscape();
-
-    /// Read [PlayerData] and [Settings] from hive.
     playerData = await _readPlayerData();
     settings = await _readSettings();
-
-    /// Initilize [AudioManager].
     await AudioManager.instance.init(_audioAssets, settings);
-
-    // Start playing background music. Internally takes care
-    // of checking user settings.
     AudioManager.instance.startBgm('8BitPlatformerLoop.wav');
-
-    // Cache all the images.
     await images.loadAll(_imageAssets);
-
-    // This makes the camera look at the center of the viewport.
     camera.viewfinder.position = camera.viewport.virtualSize * 0.5;
+    _addParallaxBackground();
+  }
 
-    /// Create a [ParallaxComponent] and add it to game.
+  Future<void> _addParallaxBackground() async {
     final parallaxBackground = await loadParallaxComponent(
       [
-        ParallaxImageData('parallax/plx-1.png'),
-        ParallaxImageData('parallax/plx-2.png'),
-        ParallaxImageData('parallax/plx-3.png'),
-        ParallaxImageData('parallax/plx-4.png'),
-        ParallaxImageData('parallax/plx-5.png'),
-        ParallaxImageData('parallax/plx-6.png'),
+        for (int i = 1; i <= 6; i++) ParallaxImageData('parallax/plx-\$i.png')
       ],
       baseVelocity: Vector2(10, 0),
       velocityMultiplierDelta: Vector2(1.4, 0),
     );
-
-    // Add the parallax as the backdrop.
     camera.backdrop.add(parallaxBackground);
   }
 
-  /// This method add the already created [Dino]
-  /// and [EnemyManager] to this game.
   void startGamePlay() {
     _dino = Dino(images.fromCache('DinoSprites - tard.png'), playerData);
     _enemyManager = EnemyManager();
-
-    world.add(_dino);
-    world.add(_enemyManager);
+    world.addAll([_dino, _enemyManager]);
   }
 
-  // This method remove all the actors from the game.
   void _disconnectActors() {
     _dino.removeFromParent();
     _enemyManager.removeAllEnemies();
     _enemyManager.removeFromParent();
   }
 
-  // This method reset the whole game world to initial state.
   void reset() {
-    // First disconnect all actions from game world.
     _disconnectActors();
-
-    // Reset player data to inital values.
     playerData.currentScore = 0;
     playerData.lives = 5;
   }
 
-  // This method gets called for each tick/frame of the game.
   @override
   void update(double dt) {
-    // If number of lives is 0 or less, game is over.
     if (playerData.lives <= 0) {
       overlays.add(GameOverMenu.id);
       overlays.remove(Hud.id);
@@ -130,74 +102,36 @@ class DinoRun extends FlameGame with TapDetector, HasCollisionDetection {
     super.update(dt);
   }
 
-  // This will get called for each tap on the screen.
   @override
   void onTapDown(TapDownInfo info) {
-    // Make dino jump only when game is playing.
-    // When game is in playing state, only Hud will be the active overlay.
     if (overlays.isActive(Hud.id)) {
       _dino.jump();
     }
     super.onTapDown(info);
   }
 
-  /// This method reads [PlayerData] from the hive box.
   Future<PlayerData> _readPlayerData() async {
-    final playerDataBox =
-        await Hive.openBox<PlayerData>('DinoRun.PlayerDataBox');
-    final playerData = playerDataBox.get('DinoRun.PlayerData');
-
-    // If data is null, this is probably a fresh launch of the game.
-    if (playerData == null) {
-      // In such cases store default values in hive.
-      await playerDataBox.put('DinoRun.PlayerData', PlayerData());
-    }
-
-    // Now it is safe to return the stored value.
-    return playerDataBox.get('DinoRun.PlayerData')!;
+    final box = await Hive.openBox<PlayerData>('DinoRun.PlayerDataBox');
+    return box.get('DinoRun.PlayerData') ?? PlayerData();
   }
 
-  /// This method reads [Settings] from the hive box.
   Future<Settings> _readSettings() async {
-    final settingsBox = await Hive.openBox<Settings>('DinoRun.SettingsBox');
-    final settings = settingsBox.get('DinoRun.Settings');
-
-    // If data is null, this is probably a fresh launch of the game.
-    if (settings == null) {
-      // In such cases store default values in hive.
-      await settingsBox.put(
-        'DinoRun.Settings',
-        Settings(bgm: true, sfx: true),
-      );
-    }
-
-    // Now it is safe to return the stored value.
-    return settingsBox.get('DinoRun.Settings')!;
+    final box = await Hive.openBox<Settings>('DinoRun.SettingsBox');
+    return box.get('DinoRun.Settings') ?? Settings(bgm: true, sfx: true);
   }
 
   @override
   void lifecycleStateChange(AppLifecycleState state) {
-    switch (state) {
-      case AppLifecycleState.resumed:
-        // On resume, if active overlay is not PauseMenu,
-        // resume the engine (lets the parallax effect play).
-        if (!(overlays.isActive(PauseMenu.id)) &&
-            !(overlays.isActive(GameOverMenu.id))) {
-          resumeEngine();
-        }
-        break;
-      case AppLifecycleState.paused:
-      case AppLifecycleState.detached:
-      case AppLifecycleState.inactive:
-      case AppLifecycleState.hidden:
-        // If game is active, then remove Hud and add PauseMenu
-        // before pausing the game.
-        if (overlays.isActive(Hud.id)) {
-          overlays.remove(Hud.id);
-          overlays.add(PauseMenu.id);
-        }
-        pauseEngine();
-        break;
+    if (state == AppLifecycleState.paused || state == AppLifecycleState.inactive) {
+      if (overlays.isActive(Hud.id)) {
+        overlays.remove(Hud.id);
+        overlays.add(PauseMenu.id);
+      }
+      pauseEngine();
+    } else if (state == AppLifecycleState.resumed && 
+               !overlays.isActive(PauseMenu.id) && 
+               !overlays.isActive(GameOverMenu.id)) {
+      resumeEngine();
     }
     super.lifecycleStateChange(state);
   }
